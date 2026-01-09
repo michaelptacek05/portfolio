a399bf600a062f61753bf3dcccdb2ea987ec7314
Michael Ptáček <mptacek005@gmail.com>
2026-01-09 09:54:07 +0100

    exprerience change

--- app/constants/experience.tsx
+++ app/constants/experience.tsx
@@ -13,7 +13,7 @@ export const EXPERIENCES: ExperienceItem[] = [
     role: "UX/UI designer / Frontend Developer",
     type: "Freelance",
     company: "MagicWare",
-    period: "2024 - současnost",
+    period: "Únor 2024 - současnost",
     description: "V MagicWare analyzuji a tvořím interaktivní wireframes webových i mobilních aplikací, stránek a systémů, vylepšuji staré weby a optimalizuji SEO a přístupnost, tvořím a designuji řadu tiskových produktů od brožur, vizitek, log a mockupů a řeším systémové chyby, které se objevují.",
     skills: ["Figma", "Adobe Suite", "HTML", "CSS", "SASS", "JavaScript ES6+", "React", "Tailwind", "Testing", "SEO"]
 
@@ -23,7 +23,7 @@ export const EXPERIENCES: ExperienceItem[] = [
     type: "Freelance",
     company: "Freelance",
     period: "2023 - současnost",
-    description: "Tvorba webových stránek s optimalizovaným SEO a tvorba grafických návrhu.",
-    skills: ["Figma", "Adobe Suite", "HTML", "CSS", "React", "Astro.js", "Tailwind", "SEO"]
+    description: "Tvorba webových stránek s důrazem na SEO, grafický design a hloubková analýza uživatelského prožitku (UX).",
+    skills: ["Figma", "Adobe Suite", "HTML", "CSS", "React", "Astro.js", "Tailwind", "SEO", "MS Clarity"]
   },
 ];
